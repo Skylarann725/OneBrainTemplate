6dc790859fad70e7b6ec4da7b35d0fda961935b9
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2018-08-09 19:54:11 -0400

    fixing email

--- php/form-process.php
+++ php/form-process.php
@@ -32,7 +32,7 @@ if (empty($_POST["message"])) {
 }
 
 
-$EmailTo = "skylarsiagel@yahoo.com";
+$EmailTo = "Tutor@OneBrainLearningCenter.com";
 $Subject = "New Message Received";
 
 // prepare email body text
